91ae3370df62da9daa56fa7eba89560bda0ebc5e
iampanya <panya.cs13@gmail.com>
2017-02-20 23:32:59 +0700

    Create new table : profile

---
+++ database/migrations/2017_02_20_161043_create_profile_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('titlename');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('hometown');
+            $table->string('livetown');
+            $table->string('school');
+            $table->string('work');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
