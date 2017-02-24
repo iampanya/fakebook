5ab80ba27d68cb9ee76ddfee3d228905da155758
iampanya <panya.cs13@gmail.com>
2017-02-24 19:42:59 +0700

    Add layout for profile page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,7 +9,7 @@ class ProfileController extends Controller
 {
     //
     function index(){
-      return view('profile.index');
+      return view('profile.index')->with('name', 'Panya');
     }
 
     function edit() {
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -2,9 +2,30 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-      <a href="{{url('/profile/edit')}}" class="pull-right">Edit Profile</a>
+  <div class="row">
+    <a href="{{url('/profile/edit')}}" class="pull-right">Edit Profile</a>
+  </div>
+  <h1>Profile index</h1>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">My Profile</h3>
+        </div>
+        <div class="panel-body">
+          <ul class="list-group">
+            <li class="list-group-item">Name: {{ $name }}</li>
+            {{-- <li class="list-group-item">School: {{ school }}</li>
+            <li class="list-group-item">Work: {{ work }}</li>
+            <li class="list-group-item">Hometown: {{ hometown }}</li>
+            <li class="list-group-item">Live in: {{ livein }}</li> --}}
+          </ul>
+        </div>
+      </div>
     </div>
-    <h1>Profile index</h1>
+    <div class="col-md-6">
+      <h2>My Post</h2>
+    </div>
+  </div>
 </div>
 @endsection
